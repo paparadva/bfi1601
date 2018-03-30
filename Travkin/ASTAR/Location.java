349e60aeddaff7270a1b39a2c6e0d62446717b8d
asassinev <asassinev@yandex.ru>
2018-03-30 13:57:45 +0300

    Добавил папку ASTAR с лабораторная работой №2

---
+++ Travkin/ASTAR/Location.java
@@ -0,0 +1,39 @@
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+    public boolean equals (Object o){
+        if (this == o) return true;
+        if ((o==null)||getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return xCoord == location.xCoord && yCoord == location.yCoord;
+    }
+
+    public int hashCode() {
+        int hash = 17;
+        hash = 31 * hash + xCoord;
+        hash = 31 * hash + yCoord;
+        return hash;
+    }
+}
